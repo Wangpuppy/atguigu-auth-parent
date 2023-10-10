e44d823113c7c7a3edb7e381b728a507b1e2586d
Ruiyang Wang <895437987@qq.com>
2023-10-10 16:35:01 +0800

    feat: 用户登录模块

---
+++ atguigu-auth/service-system/src/main/java/com/atguigu/system/controller/IndexController.java
@@ -0,0 +1,45 @@
+package com.atguigu.system.controller;
+
+import com.atguigu.system.result.Result;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description 用户登录
+ * @Author wrystart
+ * @Version
+ * @Date 2023/10/10 16:28
+ */
+@Api(tags = "用户登录")
+@RestController
+@RequestMapping("/admin/system/index")
+public class IndexController {
+
+    //login
+    @PostMapping("login")
+    public Result login123() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", "admin-token atguigu");
+        return Result.ok(map);
+    }
+
+    //info
+    @GetMapping("info")
+    public Result info() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("roles", "[ admin]");
+        map.put("introduction", "I am a super administrator");
+        map.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        map.put("name", "Super Admin atguigu");
+        return Result.ok(map);
+    }
+
+
+
+}
