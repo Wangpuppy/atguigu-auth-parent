bde71ff7a979611ebbc19754b440838940950027
Ruiyang Wang <895437987@qq.com>
2023-10-10 15:54:14 +0800

    feat: 添加MybatisPlus配置类,添加分页拦截器

---
+++ atguigu-auth/common/service-util/src/main/java/com/atguigu/system/config/MybatisPlusConfig.java
@@ -0,0 +1,30 @@
+package com.atguigu.system.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description MybatisPlus配置类
+ * @Author wrystart
+ * @Version
+ * @Date 2023/10/10 15:47
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor addPaginationInnerInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //向Mybatis过滤器链中添加分页拦截器
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
