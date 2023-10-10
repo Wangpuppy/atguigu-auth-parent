710db00f1a9e008764a51b5ffc68e3cbdc6482ba
Ruiyang Wang <895437987@qq.com>
2023-10-10 16:16:36 +0800

    feat: 全局异常处理

---
+++ atguigu-auth/common/service-util/src/main/java/com/atguigu/system/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.atguigu.system.exception;
+
+import com.atguigu.system.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description 全局异常处理类
+ * @Author wrystart
+ * @Version
+ * @Date 2023/10/10 16:12
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //1 全局异常
+    @ExceptionHandler(Exception.class) //异常处理器
+    @ResponseBody
+    public Result error(Exception e){
+        System.out.println("全局....");
+        e.printStackTrace();
+        return Result.fail().message("执行了全局异常处理");
+    }
+
+    //2 特定异常处理
+    @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody
+    public Result error(ArithmeticException e){
+        System.out.println("特定....");
+        e.printStackTrace();
+        return Result.fail().message("执行了特定异常处理");
+    }
+}
