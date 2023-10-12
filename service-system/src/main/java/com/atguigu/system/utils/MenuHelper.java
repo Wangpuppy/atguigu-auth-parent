0ca18d3a19847e6e9fb16b1f7e5f0d434f7a76dd
Ruiyang Wang <895437987@qq.com>
2023-10-12 21:02:54 +0800

    feat: 菜单工具类

---
+++ service-system/src/main/java/com/atguigu/system/utils/MenuHelper.java
@@ -0,0 +1,57 @@
+package com.atguigu.system.utils;
+
+import com.atguigu.model.system.SysMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author wrystart
+ * @Version
+ * @Date 2023/10/12 20:17
+ */
+public class MenuHelper {
+
+
+    //构建树形结构
+    public static List<SysMenu> buildTree(List<SysMenu> sysMenuList) {
+        //创建集合封装最终数据
+        List<SysMenu> trees = new ArrayList<SysMenu>();
+
+        //遍历所有菜单的集合
+        for (SysMenu sysMenu:sysMenuList) {
+            //找到递归的入口,parentid=0
+            if (sysMenu.getParentId().longValue() == 0) {
+                trees.add(findChildren(sysMenu,sysMenuList));
+            }
+        }
+        return trees;
+    }
+
+    //从根节点进行递归查询,查询子节点
+    // 判断id = parentid是否相同,如果相同是子节点,进行数据封装
+    private static SysMenu findChildren(SysMenu sysMenu, List<SysMenu> treeNodes) {
+        //数据初始化
+        sysMenu.setChildren(new ArrayList<SysMenu>());
+
+        //遍历递归查找
+        for (SysMenu it :treeNodes){
+            //获取当前菜单id
+//            String id = sysMenu.getId();
+//            long cid = Long.parseLong(id);
+
+            //获取所有菜单的parentid
+//            Long parentId = it.getParentId();
+
+            //比对
+            if (Long.parseLong(sysMenu.getId()) ==it.getParentId()){
+                if (sysMenu.getChildren()==null){
+                    sysMenu.setChildren(new ArrayList<SysMenu>());
+                }
+                sysMenu.getChildren().add(findChildren(it,treeNodes));
+            }
+        }
+        return sysMenu;
+    }
+}
